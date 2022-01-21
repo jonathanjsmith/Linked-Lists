db9360a7c8dfb105374d3d1ccec803e4487dad96
jonathanjsmith <63128850+jonathanjsmith@users.noreply.github.com>
2022-01-21 12:21:35 -0600

    Add files via upload

--- ispalindrome.py
+++ ispalindrome.py
@@ -9,37 +9,25 @@ class Node:
         self.val = val
         self.next = next
 
-# return the second half of the list after middle node
-def getSecondHalf(head):
+def isPalindrome(head):
+    
+    # reverse the first half the list
+    rev = None
     slow = fast = head
     while fast and fast.next:
-        slow = slow.next
         fast = fast.next.next
-    return slow
-
-# reverse linked list
-def reverseList(head):
-    prev = None
-    cur = head
-    while cur:
-        t = cur.next
-        cur.next = prev
-        prev = cur
-        cur = t
-    return prev
-
-# check whether list is palindrome
-def isPalindrome(head):
-    # get the second half of the list and reverse it
-    m = getSecondHalf(head)
-    m = reverseList(m)
+        rev, slow.next, slow = slow, rev, slow.next
     
-    # compare both lists
-    while head and m:
-        if head.val != m.val:
+    # case: middle node
+    if fast:
+        slow = slow.next
+    
+    # compare each node in first half of list (reversed) and second half
+    while rev:
+        if rev.val != slow.val:
             return False
-        head = head.next
-        m = m.next
+        rev, slow = rev.next, slow.next
+        
     return True
 
 """
@@ -47,18 +35,14 @@ Time: O(n)
 Space: O(1)
 """
 
-list_1 = Node(1)
-list_1.next = Node(2)
-list_1.next.next = Node(2)
-list_1.next.next.next = Node(1)
+input_1 = Node(1)
+input_1.next = Node(2)
+input_1.next.next = Node(2)
+input_1.next.next.next = Node(1)
+ans = isPalindrome(input_1)
+print(ans)
 
-list_2 = Node(1)
-list_2.next = Node(2)
-list_2.next.next = Node(1)
-
-list_3 = Node(1)
-list_3.next = Node(2)
-
-print(isPalindrome(list_1))
-print(isPalindrome(list_2))
-print(isPalindrome(list_3))
+input_2 = Node(1)
+input_2.next = Node(2)
+ans = isPalindrome(input_2)
+print(ans)
